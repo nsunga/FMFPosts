0e73dce93c273ab601349a3f981b94d60113510a
Nicholas Sunga <nick.sunga95@gmail.com>
2018-02-13 18:43:42 -0800

    can get new titles and understanding urls

--- app.py
+++ app.py
@@ -3,25 +3,45 @@ import praw
 from tweepy_keys_and_tokens import *
 from praw_keys_and_tokens import *
 
-twit_auth = tweepy.OAuthHandler(twit_consumer_key, twit_consumer_secret)
-twit_auth.set_access_token(twit_access_token, twit_token_secret)
+# twit_auth = tweepy.OAuthHandler(twit_consumer_key, twit_consumer_secret)
+# twit_auth.set_access_token(twit_access_token, twit_token_secret)
+#
+# twit_api = tweepy.API(twit_auth)
 
-twit_api = tweepy.API(twit_auth)
+# reddit = praw.Reddit(client_id=redd_client_id,
+#                      client_secret=redd_client_secret,
+#                      password=redd_password,
+#                      user_agent=redd_user_agent,
+#                      username=redd_username)
 
-reddit = praw.Reddit(client_id=redd_client_id,
-                     client_secret=redd_client_secret,
-                     password=redd_password,
-                     user_agent=redd_user_agent,
-                     username=redd_username)
-# print(redd_client_id)
-# print(redd_client_secret)
-# print(redd_password)
-# print(redd_user_agent)
-# print(redd_username)
+# print(reddit.user.me())
 
-print(reddit.user.me())
-# print(reddit.redditor(redd_username))
-print('done')
+def connect_to_reddit():
+    return praw.Reddit(client_id=redd_client_id,
+                         client_secret=redd_client_secret,
+                         password=redd_password,
+                         user_agent=redd_user_agent,
+                         username=redd_username)
+
+def get_titles(reddit, subreddit):
+    title_list = []
+    for every_submission in reddit.subreddit('frugalmalefashion').new(limit=5):
+        title_list.append(every_submission.title)
+        print("PRINTING URLS", every_submission.url)
+
+    return title_list
+
+def main():
+    reddit = connect_to_reddit()
+
+    print(reddit.user.me())
+    print('reddit credentials good')
+
+    tweet_list = get_titles(reddit, reddit.subreddit('frugalmalefashion'))
+
+    for every_title in tweet_list:
+        print(every_title + '\n')
+
 
 # public_tweets = api.home_timeline()
 # for tweet in public_tweets:
@@ -29,3 +49,5 @@ print('done')
 
 # api.update_status('first tweet')
 # print('done')
+if __name__ == '__main__':
+    main()
