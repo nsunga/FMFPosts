44ff118edc10ae7e20f5347f2cc2f8ec5daa2c7e
Nicholas Sunga <nick.sunga95@gmail.com>
2018-02-14 09:44:20 -0800

    clean up and TODO: filtering

--- app.py
+++ app.py
@@ -3,19 +3,6 @@ import praw
 from tweepy_keys_and_tokens import *
 from praw_keys_and_tokens import *
 
-# twit_auth = tweepy.OAuthHandler(twit_consumer_key, twit_consumer_secret)
-# twit_auth.set_access_token(twit_access_token, twit_token_secret)
-#
-# twit_api = tweepy.API(twit_auth)
-
-# reddit = praw.Reddit(client_id=redd_client_id,
-#                      client_secret=redd_client_secret,
-#                      password=redd_password,
-#                      user_agent=redd_user_agent,
-#                      username=redd_username)
-
-# print(reddit.user.me())
-
 def connect_to_reddit():
     return praw.Reddit(client_id=redd_client_id,
                          client_secret=redd_client_secret,
@@ -23,11 +10,13 @@ def connect_to_reddit():
                          user_agent=redd_user_agent,
                          username=redd_username)
 
-# TODO: Instead use a dictionary -> INDEX : (TITLE, URL)
+# TODO: FILTERING
 def title_url_dictionary(reddit, subreddit):
     title_and_url = {}
     for every_submission in reddit.subreddit('frugalmalefashion').new(limit=5):
-        title_and_url[every_submission.title] = every_submission.url
+        if every_submission.score > 15:
+            title_and_url[every_submission.title] = every_submission.url
+            print(every_submission.score)
 
     return title_and_url
 
@@ -46,20 +35,10 @@ def main():
     tweet_list = title_url_dictionary(reddit, reddit.subreddit('frugalmalefashion'))
     twit_api = connect_to_twitter()
 
-    for every_key, every_value in tweet_list.items():
-        print(every_key, every_value)
-        twit_api.update_status(every_key + ' ' + every_value)
-        break
-    # twit_api.update_status(tweet_list[0] +)
-    # for every_title in tweet_list:
-    #     print(every_title + '\n')
+    # for every_key, every_value in tweet_list.items():
+    #     print(every_key, every_value)
+    #     twit_api.update_status(every_key + ' ' + every_value)
+    #     break
 
-
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#     print(tweet.text + '\n')
-
-# api.update_status('first tweet')
-# print('done')
 if __name__ == '__main__':
     main()
